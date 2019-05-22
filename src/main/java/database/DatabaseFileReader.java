e9912dc7c4aceace50cd8973a8c6bcbcc4b2cb5e
Dylan Davis <djdavis1420@gmail.com>
2019-05-22 16:58:51 -0500

    Can Get Raw List of Products from Database File

---
+++ src/main/java/database/DatabaseFileReader.java
@@ -0,0 +1,25 @@
+package database;
+
+import com.opencsv.CSVReader;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class DatabaseFileReader {
+
+    private static final String PRODUCT_DATABASE = "./src/main/java/database/ProductDatabase.csv";
+
+    public List<String[]> getProductListing() {
+        try {
+            Reader reader = Files.newBufferedReader(Paths.get(PRODUCT_DATABASE));
+            CSVReader csvReader = new CSVReader(reader);
+            return csvReader.readAll();
+        } catch (IOException ie) {
+            ie.printStackTrace();
+        }
+        return null;
+    }
+}
---
+++ src/test/java/database/DatabaseFileReaderTest.java
@@ -0,0 +1,24 @@
+package database;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class DatabaseFileReaderTest {
+
+    private DatabaseFileReader DatabaseFileReader;
+
+    @Before
+    public void setup() {
+        DatabaseFileReader = new DatabaseFileReader();
+    }
+
+    @Test
+    public void getProductListing_shouldRetrieveProductListingFromDatabaseFile() {
+        List<String[]> databaseListing = DatabaseFileReader.getProductListing();
+        List<String> productNames = databaseListing.stream().map(x -> x[0]).collect(Collectors.toList());
+        assert productNames.contains("Snickers");
+    }
+}
