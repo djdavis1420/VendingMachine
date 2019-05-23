4e1f897832a9397ad76fe9dfdb183c5465b6985b
Dylan Davis <djdavis1420@gmail.com>
2019-05-23 15:04:37 -0500

    ProductsTable Returns Empty List When No Products At Location

--- src/main/java/com/database/ProductsTable.java
+++ src/main/java/com/database/ProductsTable.java
@@ -2,10 +2,12 @@ package com.database;
 
 import com.models.Product;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collections;
 import java.util.List;
 
 @Repository
@@ -17,6 +19,10 @@ public class ProductsTable {
     private static final String SELECT_PRODUCT = "SELECT * FROM products WHERE location = ?";
 
     public List<Product> selectProductByLocation(String productLocation) {
+        try {
             return jdbcTemplate.query(SELECT_PRODUCT, new Object[]{productLocation}, new BeanPropertyRowMapper<>(Product.class));
+        } catch (EmptyResultDataAccessException ex) {
+            return Collections.emptyList();
+        }
     }
 }
--- src/test/java/com/database/ProductsTableIT.java
+++ src/test/java/com/database/ProductsTableIT.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Collections;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
@@ -28,5 +29,13 @@ public class ProductsTableIT {
             long actualCount = actual.stream().filter(x -> x.getName().equals("Snickers")).count();
 
             assertEquals(expectedCount, actualCount);
+    }
+
+    @Test
+    public void selectProductByLocation_shouldReturnEmptyListWhenProductNotFound() {
+        String productLocation = "G2";
+        List expected = Collections.emptyList();
+        List<Product> actual = productsTable.selectProductByLocation(productLocation);
+        assertEquals(expected, actual);
     }
 }
