248854c7926071aa89956e0e2c0da6d045fb7752
Dylan Davis <djdavis1420@gmail.com>
2019-05-29 13:31:50 -0500

    Purchase Endpoint Successfully Returns Transaction Object

--- src/test/java/com/controllers/RESTControllerTest.java
+++ src/test/java/com/controllers/RESTControllerTest.java
@@ -2,6 +2,7 @@ package com.controllers;
 
 import com.models.Coin;
 import com.models.ProductRequest;
+import com.models.Transaction;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -9,8 +10,8 @@ import java.util.Arrays;
 import java.util.List;
 
 import static com.models.Coin.*;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
 import static org.mockito.internal.verification.VerificationModeFactory.times;
 
 public class RESTControllerTest {
@@ -39,4 +40,16 @@ public class RESTControllerTest {
         verify(vendingMachineController, times(1)).processTransaction(AVAILABLE_SELECTION, coins);
     }
 
+    @Test
+    public void purchase_shouldReturnTransaction() {
+        List<Coin> coins = Arrays.asList(DOLLAR, QUARTER, DIME, NICKEL);
+        productRequest.setProductLocation(AVAILABLE_SELECTION);
+        productRequest.setInsertedCoins(coins);
+        Transaction expectedTransaction = new Transaction();
+        when(vendingMachineController.processTransaction(AVAILABLE_SELECTION, coins)).thenReturn(expectedTransaction);
+
+        Transaction actualTransaction = restController.purchase(productRequest);
+
+        assertEquals(expectedTransaction, actualTransaction);
+    }
 }
